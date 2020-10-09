9483e2067883398b748857b34cb6ed46db59a6af
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-09 12:13:08 +0000

    Puts values into array

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.utl.Scanner;
 /**
- *
- * @author 
+ * Asks for Fibonacci number and tells you its value
+ * @author Liana Bazzarella 
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user what Fibonacci they want to find
+    System.out.println("What Fibonacci number would you like to find");
+    // create a variable for user input 
+    int fibNumber = input.nextInt();
+
+    // create constant for array spots
+    final int LAST_NUMB = fibNumber;
+    
+    // create an array
+    int[] sequence = new int[LAST_NUMB];
+
+    // put values into the array using a loop
+    for(int i = 0; i < LAST_NUMB; i++){
+      sequence[i]= input.nextInt();
+    }
+
+
     
   }
 }
